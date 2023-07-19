3978c3901a27bc4be5cb4e735d038a6d2f24d067
crashmax-dev <crashmax.dev@gmail.com>
2023-07-19 10:14:07 +0000

    Deploying to gh-pages from @ gigachad-dev/7tv-userscript@072fa089f88298ede7f2e3f2f60c869c1c17e551 🚀

---
+++ 7tv-userscript.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        7tv-userscript
+// @version     0.1.0
+// @author      crashmax <userscript@crashmax.ru>
+// @license     MIT
+// @homepage    https://gigachad-dev.github.io/7tv-userscript/
+// @match       https://7tv.app/*
+// @grant       GM_addStyle
+// @updateURL   https://gigachad-dev.github.io/7tv-userscript/7tv-userscript.meta.js
+// @downloadURL https://gigachad-dev.github.io/7tv-userscript/7tv-userscript.user.js
+// ==/UserScript==
